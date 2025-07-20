fdb4706f99b0ad4685c7e6bf04912565ea504f66
Nur Rochman <nurrochman954@gmail.com>
2025-07-20 23:09:02 +0700

    Update page infografis

---
+++ app/(root)/infografis/wilayah/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React from "react";
+import Header from "@/components/Header";
+import TopBar from "@/components/TopBar";
+import Footer from "@/components/Footer";
+import InfografisSecondBar from "@/components/InfografisSecondBar";
+
+const Wilayah: React.FC = () => {
+  return (
+    <main className="min-h-screen">
+      <Header />
+      <TopBar />
+      <InfografisSecondBar />
+      <Footer />
+
+    </main>
+  );
+};
+
+
+export default Wilayah;
