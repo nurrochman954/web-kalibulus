66b11c493e11ac2a41ba1034980b502ccce663a9
Nur Rochman <nurrochman954@gmail.com>
2025-07-28 21:28:03 +0700

    Update page profil-dusun

--- app/(root)/profil-dusun/page.tsx
+++ app/(root)/profil-dusun/page.tsx
@@ -4,16 +4,19 @@ import React from "react";
 import Header from "@/components/Header";
 import TopBar from "@/components/TopBar";
 import Footer from "@/components/Footer";
+import ProfilDusun from "@/components/ProfilDusun";
 
-const ProfilDusun: React.FC = () => {
+
+const ProfilDusunPage: React.FC = () => {
   return (
     <main className="min-h-screen">
       <Header />
       <TopBar />
+      <ProfilDusun />
       <Footer />
     </main>
   );
 };
 
 
-export default ProfilDusun;
+export default ProfilDusunPage;
